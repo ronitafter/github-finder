614b723c56a77eabe92fa342afe5898795cc5360
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-02-26 12:05:20 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+// function App() {
+//   return (
+//     <div className="bg-purple-500">
+//       <h3 className="text-3xl font-bold underline">github finder</h3>
+//       <button className="btn">this is a button</button>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h3>github finder</h3>
     </div>
   );
 }
 
 export default App;
+
