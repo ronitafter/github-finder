d27fae6df234dd8c1beef22f14b9dbfbf6e1ec89
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-02-26 13:26:13 +0200

    4

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 function NavBar({title}) {
    return(
    <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
-      <div className="container mx-auto"></div>
+         <div className="md:container md:mx-auto"></div>
          <div className='flex-none px-2 mx-2'>
             <FaGithub className='inline pr-2 text-3xl' />
             <Link to='/' className='text-lg font-bold align-middle'>
