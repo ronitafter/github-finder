d5e71cde6453ed09f398313e45463bd803646a4f
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-04-16 12:49:56 +0300

    more map bugs

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -6,40 +6,21 @@ import GitHubContext from '../../context/GitHub/GitHubContext'
 import Spinner from '../layout/Spinner'
 import UserItem from './UserItem'
 
+
 function UserResults() {
-	//	const { users, loading, fetchUsers } = useContext
 	const { users, loading } = useContext(GitHubContext)
-	// const [users, setUsers] = useState([])
-	// const [loading, setLoading] = useState(true)
 
-	// useEffect(() => {
-	// 	fetchUsers()
-	// }, [])
-
-	// const fetchUsers = async () => {
-	// 	const response = await fetch(`${process.env.REACT_APP_GIT_URL}/users`, {
-	// 	})
-
-	// 	const data = await response.json()
-	// 	setUsers(data);
-	// 	setLoading(false);
-
-	// }
 	if (!loading) {
 		return (
-			<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols=2'>{users.map((user) => (
-				// <h3>{user.login}</h3>
-				<UserItem key={user.id} user={user} />
-			))}
+			<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+				{users.map((user) => (
+					<UserItem key={user.id} user={user} />
+				))}
 			</div>
 		)
-
 	} else {
 		return <Spinner />
 	}
-
-
 }
-
 
 export default UserResults
--- src/context/GitHub/GitHubContext.js
+++ src/context/GitHub/GitHubContext.js
@@ -62,7 +62,15 @@ export const GithubProvider = ({ children }) => {
   const getUserRepos = async (login) => {
     setLoading();
 
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos`, {});
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos/${params}`,
+      {}
+    );
 
     const data = await response.json();
     // setUsers(data);
