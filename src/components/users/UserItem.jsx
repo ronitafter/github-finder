f99aacb43354da5f3ac79510f25ae852d914f67e
ronitafter <54575125+ronitafter@users.noreply.github.com>
2022-04-16 15:09:37 +0300

    ;;;

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
+
 function UserItem({ user: { login, avatar_url } }) {
 	return (
 		<div className='shadow-md card compact side bg-base-100'>
 			<div className='flex-row items-center space-x-4 card-body'>
 				<div>
-					<div className="avatar">
-						<div className="rounded-full shadow w-14 h-14">
-							<img src={avatar_url} alt="avatar" />
+					<div className='avatar'>
+						<div className='rounded-full shadow w-14 h-14'>
+							<img src={avatar_url} alt='Profile' />
 						</div>
 					</div>
 				</div>
 				<div>
 					<h2 className='card-title'>{login}</h2>
-					<Link className='text-base-content text-opacity-40'
-						to={`/user/${login}`}>Visit Profile</Link>
+					<Link
+						className='text-base-content text-opacity-40'
+						to={`/user/${login}`}
+					>
+						Visit Profile
+					</Link>
 				</div>
 			</div>
 		</div>
@@ -26,4 +31,5 @@ function UserItem({ user: { login, avatar_url } }) {
 UserItem.propTypes = {
 	user: PropTypes.object.isRequired,
 }
-export default UserItem;
+
+export default UserItem
